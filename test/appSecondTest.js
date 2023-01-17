b53d0cd839a998105b7c4158d004a8593d57a0f4
Felippe Alves de Paiva <felippeapaiva@gmail.com>
2023-01-17 10:08:42 -0800

    added mocha for testing. Created other files to run parallel test. Mochawesome for reporting.

---
+++ test/appSecondTest.js
@@ -0,0 +1,46 @@
+// test script using mocha and node.
+
+const { Builder, By, Key } = require('selenium-webdriver');
+const assert = require('assert');
+
+const imageOnePiece =
+  'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTyklyDoHod9SEqahgN9_dSwv0wyT_KH0oQlg&usqp=CAU';
+
+describe('Adding movie to the list', function () {
+  it('should add a movie to the list', async function () {
+    // launch browser
+    let driver = await new Builder().forBrowser('chrome').build();
+
+    // navigate to app
+    await driver.get('http://127.0.0.1:5500/');
+
+    // add a movie to the list
+    const addMovieButton = await driver.findElement(
+      By.xpath("//button[text()='ADD MOVIE']")
+    );
+    await addMovieButton.click();
+
+    await driver.findElement(By.id('title')).sendKeys('One Piece');
+    await driver.findElement(By.name('image-url')).sendKeys(imageOnePiece);
+    await driver.findElement(By.name('rating')).sendKeys('5');
+
+    const confirmAddMovieButton = await driver.findElement(
+      By.xpath("//button[text()='Add']")
+    );
+    await confirmAddMovieButton.click();
+
+    // assert
+    // finding the added movie and saving it to the addMovie variable.
+    let addedMovie = await driver
+      .findElement(By.xpath('//*[@id="movie-list"]/li/div[2]/h2'))
+      .getText()
+      .then(function (value) {
+        return value;
+      });
+    // assert using node assertion
+    assert.strictEqual(addedMovie, 'One Piece');
+
+    // close browser
+    await driver.quit();
+  });
+});
